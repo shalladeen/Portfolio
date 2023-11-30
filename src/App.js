ef5cf35a3ab7b1c529d6de728c29e4491eb60502
Shawna Halladeen <shawnakatrina.halladeen@gmail.com>
2023-11-30 01:05:25 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import "@fontsource/comfortaa";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <body>
+      <header>
+        <nav>
+          <li>Home</li>
+          <li>Koda</li>
+        </nav>
+       </header>
+        <p className="greeting">Hi there, <strong className="name">I'm Shawna!</strong></p>
+     </body>
     </div>
   );
 }
