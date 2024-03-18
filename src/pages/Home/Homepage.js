3c95e6ffa3686cf522fee7ea73c3e1944306f4f6
Shawna Halladeen <shawnakatrina.halladeen@gmail.com>
2024-03-18 02:13:46 -0400

    Added new svgs to project

--- src/pages/Home/Homepage.js
+++ src/pages/Home/Homepage.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import './Home.css'
-import test from '../../images/test1.svg';
-import { FaAngleDown } from 'react-icons/fa'
-
+import React from 'react';
+import './Home.css';
+import { ReactComponent as MePortfolio } from '../../images/me-Portfolio.svg';
+import { ReactComponent as Blob } from '../../images/blob.svg';
+import { FaAngleDown } from 'react-icons/fa';
 
 const Homepage = () => {
   const scrollToProjects = () => {
@@ -13,19 +13,26 @@ const Homepage = () => {
   };
 
   return (
-    <div className = "Home">
-     <body className = "home-body">
+    <div className="Home">
+  <div className="home-body">
+    <div className="svg-and-greeting-container">
+    <div className="svg-container" >
+  <Blob className="blob-svg" />
+  <div className="me-Portfolio-clip">
+    <MePortfolio className="me-Portfolio-svg" />
+  </div>
+</div>
       <div className="home-text-container">
-      <p className="greeting">Hi there, <strong className="name">I'm Shawna!</strong></p>
-      <p className="home-desc">I am a Software Engineering Student</p>
-      <button className="projects-button" onClick={scrollToProjects}>
+        <p className="greeting">Hi there, <strong className="name">I'm Shawna!</strong></p>
+        <p className="home-desc">I am a Software Engineering Student</p>
+        <button className="projects-button" onClick={scrollToProjects}>
           My Projects <FaAngleDown />
         </button>
       </div>
-      <img src={test}/>
-   </body>
+    </div>
   </div>
-  )
-}
+</div>
+  );
+};
 
-export default Homepage
+export default Homepage;
