52ee8a5f63afb31d5090f26aaad1211582b78268
Shawna <shawnakatrina.halladeen@gmail.com>
2024-10-29 12:14:25 -0400

    changed sizes of contact icons

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Box, Heading, Flex, Link } from '@chakra-ui/react';
+import { Box, Heading, Text, Link, Flex } from '@chakra-ui/react';
 import { FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Contact = () => {
   return (
     <Box
-      bg="white"
+    bg="white"
       p="4"
       minHeight={{ base: 'auto', md: '90vh' }}
       display="flex"
@@ -13,35 +13,38 @@ const Contact = () => {
       justifyContent="center"
       alignItems="center"
       textAlign="center"
-      boxShadow="lg"
-      borderRadius="md"
     >
-      <Heading as="h2" size="xl" mb={4}>
-        Let's Connect!
-      </Heading>
-      <Flex mt="4" justifyContent="center">
-        <Link
-          href="https://www.linkedin.com/in/shalladeen"
-          isExternal
-          mx={4}
-          color="black"
-          _hover={{ color: "gray.500" }}
-          display="flex"
-          alignItems="center"
-        >
-          <FaLinkedin size="2em" />
-        </Link>
-        <Link
-          href="mailto:shawnakatrina.halladeen@gmail.com"
-          mx={4}
-          color="black"
-          _hover={{ color: "gray.500" }}
-          display="flex"
-          alignItems="center"
-        >
-          <FaEnvelope size="2em" />
-        </Link>
-      </Flex>
+      <Box pb="20px" fontSize="40px">
+        <Heading as="h1" size="2xl" mb={3}>
+          CONTACT
+        </Heading>
+        <Text fontSize="xl" mt="3">
+          Let's connect!
+        </Text>
+        <Flex mt="4" justifyContent="center">
+          <Link
+            href="https://www.linkedin.com/in/shalladeen"
+            isExternal
+            mx={2}
+            color="black"
+            _hover={{ color: "gray.300" }}
+            display="flex"
+            alignItems="center"
+          >
+            <FaLinkedin size="2em" />
+          </Link>
+          <Link
+            href="mailto:shawnakatrina.halladeen@gmail.com"
+            mx={2}
+            color="black"
+            _hover={{ color: "gray.300" }}
+            display="flex"
+            alignItems="center"
+          >
+            <FaEnvelope size="2em" />
+          </Link>
+        </Flex>
+      </Box>
     </Box>
   );
 };
